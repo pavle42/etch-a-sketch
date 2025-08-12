c2b9c3b277c0268cb0df9f2c085c802a348f0635
Pavle <pavlemilovanovic42@gmail.com>
2025-08-12 23:19:14 +0200

    Add a button for recreating the grid
    I added a button that prompts the user for the
    dimensions of the new grid, then the old grid is
    removed, and the new one with the specified dimensions
    is created.

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-const SCREEN_HEIGHT = window.innerHeight;
-const boxSize = SCREEN_HEIGHT / 16 - 2;
+const SCREEN_HEIGHT = window.innerHeight - 100;
 
-let boxes = [];
 const container = document.getElementById("container");
+const pickButton = document.getElementById("pick-button");
+pickButton.addEventListener("click", () => {
+    createNewGrid(prompt("Pick the new grid size: "));
+});
 
-for (let i = 0; i < 16; i++) {
-    let row = [];
-    for (let j = 0; j < 16; j++) {
-        const box = document.createElement("div");
-        box.style.height = boxSize + "px";
-        box.style.width = boxSize + "px";
-
-        box.addEventListener("mouseenter", () => {
-            box.style.backgroundColor = "black";
-        });
-
-        row.push(box);
+function createNewGrid(numberOfBoxes) {
+    if (numberOfBoxes > 100) {
+        alert("Please enter a number less then or equal to 100");
+        return;
     }
-    boxes.push(row);
+
+    if (numberOfBoxes % 1 != 0 || numberOfBoxes <= 0) {
+        alert("Please enter a valid number.")
+        return;
+    }
+
+    container.replaceChildren();
+
+    let boxes = [];
+    let boxSize = SCREEN_HEIGHT / numberOfBoxes;
+
+    for (let i = 0; i < numberOfBoxes; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfBoxes; j++) {
+            const box = document.createElement("div");
+            box.style.height = boxSize + "px";
+            box.style.width = boxSize + "px";
+
+            box.addEventListener("mouseenter", () => {
+                box.style.backgroundColor = "black";
+            });
+
+            row.push(box);
+        }
+        boxes.push(row);
+    }
+
+    for (let i = 0; i < boxes.length; i++) {
+        const rowDiv = document.createElement("div");
+        container.appendChild(rowDiv);
+        for (let j = 0; j < boxes[i].length; j++) {
+            rowDiv.appendChild(boxes[i][j]);
+        }
+    }
 }
 
-for (let i = 0; i < boxes.length; i++) {
-    const rowDiv = document.createElement("div");
-    container.appendChild(rowDiv);
-    for (let j = 0; j < boxes[i].length; j++) {
-        rowDiv.appendChild(boxes[i][j]);
-    }
-}
+createNewGrid(16);
